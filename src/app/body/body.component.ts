ebca200c4b76a9dd7d051db49901a2886c9eaccf
a121 <atulpisal.ap@gmail.com>
2019-08-21 22:48:05 +0530

    final commit

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -19,7 +19,11 @@ export class BodyComponent implements OnInit {
 
   constructor(private _httpService : HttpServiceService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+
+    // console.log("result is : "+await this.doTask());
+    // console.log("result2 is : "+await this.doTask2());
+    // this.doTask3();
 
     this._httpService.httpGet(this.getCountUrl).subscribe(res=>{
       console.log("response : get : ",res.status);
@@ -30,8 +34,83 @@ export class BodyComponent implements OnInit {
     },err=>{
       console.log("error : ",err);
     });
+
   }
- 
+
+  // doTask3(){
+
+  //   function Person(name) {
+  //     this.name = name;
+  //     }
+  //     Person.prototype.intro = function () {
+      
+  //     console.log("Hello I am " + this.name);
+  //     }
+  //     var person = new Person("John");
+  //     console.log("---------------- : ",person.intro());
+      
+  // }
+
+  // doTask2(){
+  //   let char="Zulwh ​d ​surjudp ​(lq ​Sbwkrq, ​MdydVfulsw ​ru ​Uxeb) ​wr ​srsxodwh ​dqg ​wkhq ​vruw ​dudqgrpob ​glvwulexwhg ​olvw ​ri ​1 ​ploolrq ​lqwhjhuv, ​hdfk ​lqwhjhu ​kdylqj ​d ​ydoxh ​>= ​1 ​dqg ​<=100 ​zlwkrxw ​xvlqj ​dqb ​exlowlq/hawhuqdo ​oleudub/ixqfwlrq ​iru ​vruwlqj.Brxu ​surjudp ​vkrxog ​fduhixoob ​frqvlghu ​wkh ​lqsxw ​dqg ​frph ​xs ​zlwk ​wkh ​prvw ​hiilflhqwvruwlqj ​vroxwlrq ​brx ​fdq ​wklqn ​ri. ​Surylgh ​wkh ​vsdfh ​dqg ​wlph ​frpsohalwb ​ri ​brxu ​dojrulwkp";
+
+  //   let arr2="";
+    
+  //   for(let i=0;i<char.length;i++){
+
+  //     if(char.charCodeAt(i)>90 && char.charCodeAt(i)<=122)
+  //     {
+  //       if(char.charCodeAt(i)+3>122)
+  //       {
+  //         arr2+=String.fromCharCode(96+(char.charCodeAt(i)+3)-122);
+  //       }
+  //       else
+  //       {
+  //         arr2+=String.fromCharCode(char.charCodeAt(i)+3);
+  //       }
+  //     }
+  //     else if(char.charCodeAt(i)<=90 && char.charCodeAt(i)>=65)
+  //     {
+  //       if(char.charCodeAt(i)+3>90)
+  //       {
+  //         arr2+=String.fromCharCode((64+(char.charCodeAt(i)+3)-90));
+  //       }
+  //       else
+  //       {
+  //         arr2+=String.fromCharCode(char.charCodeAt(i)+3);
+  //       }
+  //     }
+  //     else
+  //     {
+  //       arr2+=char.charAt(i);
+  //     }
+  //   }
+
+  //   return arr2;
+  // }
+
+  // doTask()
+  // {
+  //   let array=[1,2,3,4,5,6];
+  //   let n=10;
+  //   let temp=array.length-n;
+  //   let arr : any =[];
+    
+  //   for(let i=temp;i<array.length;i++)
+  //   {
+  //     console.log(array[i]," , ");
+  //     arr.push(array[i]);
+  //   }
+
+  //   let j=0;
+  //   for(;j<temp;j++)
+  //   {
+  //     console.log(array[j]," , ");
+  //     arr.push(array[j]);
+  //   }
+  //   return arr;
+  // }
+
   getSelectedRecords(count : any,perPage : any,callback) {
     let obj : any ={
       // startPage : Math.floor(perPage/count)-1,
